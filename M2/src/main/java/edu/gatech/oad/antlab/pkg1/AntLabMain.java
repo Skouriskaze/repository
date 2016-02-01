7b7b3796fa0f8c242245de9b0a370f3fb8cf31f0
eugeniavkim <eugeniavkim@gmail.com>
2016-01-31 21:20:03 -0500

    Edit Antlab
    Added my name and GT ID

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,8 +73,8 @@ import edu.gatech.oad.antlab.pkg2.*;
       toPrint += p4.toString("nwhitley3");
           //Person5 replace P4 with your name
           //and gburdell5 with your gt id
-          Person5 p5 = new Person5("P5");
-          toPrint += p5.toString("gburdell5");
+          Person5 p5 = new Person5("Eugenia Kim");
+          toPrint += p5.toString("ekim317");
 		  Person6 p6 = new Person6("Timothy Lai");
           toPrint += p6.toString("tlai37");
         System.out.println(toPrint);
