651fe287a162b41a4176f79254f5acdffdb6b77f
NathanWhitley <whitleynathan2@gmail.com>
2016-01-31 21:10:51 -0500

    Person4 changes
    Made changes to calc(String input).

---
+++ M2/readme.p4.txt
@@ -0,0 +1,4 @@
+Nathan Whitley
+whitleynathan2@gmail.com
+
+My favorite food is medium-rare 16 oz. ribeye.
--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -31,7 +31,11 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-      return null;
+        char[] c = input.toCharArray();
+        for (int i = 0; i < c.length; i++) {
+            c[i] = (char) (c[i] + 1);
+        }
+      return new String(c);
     }
     
     /**
